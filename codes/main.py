1664e561267f5a8303f37a35608417bc86d02824
IFFranciscoME <if.francisco.me@gmail.com>
2021-01-15 16:27:08 -0600

    comments refactor

--- codes/main.py
+++ codes/main.py
@@ -21,7 +21,7 @@ import numpy as np
 from data import ohlc_data as data
 from datetime import datetime
 
-# reproducible results
+# Reproducible results
 random.seed(123)
 
 # --------------------------------------------------------------- PLOT 1: USD/MXN OHLC HISTORICAL PRICES -- #
@@ -39,13 +39,13 @@ plot_1 =vs.g_ohlc(p_ohlc=data, p_theme=dt.theme_plot_1, p_vlines=None)
 # ---------------------------------------------------------------------- TABLE 1: SHORT DATA DESCRIPTION -- #
 # ----------------------------------------------------------------------- ------------------------------ -- #
 
-# table with data description
+# Table with data description
 table_1 = data.describe()
 
 # ------------------------------------------------------------------- TIMESERIES FOLDS FOR DATA DIVISION -- #
 # ------------------------------------------------------------------- ---------------------------------- -- #
 
-# fold size
+# Fold size
 fold_size = 'quarter'
 
 # Timeseries data division in t-folds
@@ -60,7 +60,7 @@ for fold in list(t_folds.keys()):
     dates_folds.append(t_folds[fold]['timestamp'].iloc[0])
     dates_folds.append(t_folds[fold]['timestamp'].iloc[-1])
 
-# plot_1 with t-folds vertical lines
+# Plot_1 with t-folds vertical lines
 plot_2 = vs.g_ohlc(p_ohlc=data, p_theme=dt.theme_plot_2, p_vlines=dates_folds)
 
 # Show plot in script
@@ -99,60 +99,66 @@ memory_palace = memory_palace['memory_palace']
 # -- ---------------------------------------------------------------------- PROCESS: AUC min & max cases -- #
 # -- ---------------------------------------------------------------------- ---------------------------- -- #
 
-# min and max AUC cases for the models
+# Min and max AUC cases for the models
 auc_cases = fn.model_auc(p_models=ml_models, p_global_cases=memory_palace, p_data_folds=t_folds)
 
 # -- --------------------------------------------------------------- PLOT 3: Classification Fold Results -- #
 # -- ----------------------------------------------------------------------------- --------------------- -- #
 
-# pick case
+# Pick case
 case = 'max'
 
-# pick model to generate the plot
+# Pick model to generate the plot
 auc_model = 'logistic-elasticnet'
 
-# generate title
+# Generate title
 auc_title = 'in Fold max AUC for: ' + auc_model + ' found in period: ' + \
              auc_cases[auc_model]['auc_max']['period']
 
-# plot title
+# Plot title
 dt.theme_plot_3['p_labels']['title'] = auc_title
 
-# get data from auc_cases
+# Get data from auc_cases
 train_y = auc_cases[auc_model]['auc' + '_' + case]['data']['results']['data']['train']
 test_y = auc_cases[auc_model]['auc' + '_' + case]['data']['results']['data']['test']
 
-# get data for prices and predictions
+# Get data for prices and predictions
 ohlc_prices = t_folds[auc_cases[auc_model]['auc' + '_' + case]['period']]
 ohlc_class = {'train_y': train_y['y_train'], 'train_y_pred': train_y['y_train_pred'],
               'test_y': test_y['y_test'], 'test_y_pred': test_y['y_test_pred']}
 
-# make plot
+# Make plot
 plot_3 = vs.g_ohlc_class(p_ohlc=ohlc_prices, p_theme=dt.theme_plot_3, p_data_class=ohlc_class, p_vlines=None)
 
-# visualize plot
+# Show plot in script
 # plot_3.show()
+
+# Generate plot online with chartstudio
+# py.plot(plot_3)
 
 # -- ------------------------------------------------------------------ PLOT 4: ROC and AUC Fold Results -- #
 # -- ------------------------------------------------------------------ -------------------------------- -- #
 
-# plot title
+# Plot title
 dt.theme_plot_4['p_labels']['title'] = 'in Fold max/min AUC cases'
 
 # Timeseries of the AUCs
-plot_4_folds = vs.g_roc_auc(p_cases=auc_cases, p_type='test', p_models=ml_models, p_theme=dt.theme_plot_4)
+plot_4 = vs.g_roc_auc(p_cases=auc_cases, p_type='test', p_models=ml_models, p_theme=dt.theme_plot_4)
 
-# offline plot
-plot_4_folds.show()
+# Show plot in script
+plot_4.show()
+
+# Generate plot online with chartstudio
+# py.plot(plot_4)
 
 # -- --------------------------------------------------------- Global Evaluation for AUC min & max Cases -- #
 # -- --------------------------------------------------------- ----------------------------------------- -- #
 
-# case to evaluate
+# Case to evaluate
 fold_case = 'auc_min'
-# model to evaluate
+# Model to evaluate
 fold_model = 'ann-mlp'
-# function
+# Function
 global_model = fn.global_evaluation(p_memory_palace=memory_palace, p_data=data, p_cases=auc_cases, 
                                     p_model=fold_model, p_case=fold_case)
 
@@ -168,7 +174,7 @@ global_model['model']['metrics']['test']['acc']
 # -- ------------------------------------------------------------- PLOT 5: Global Classification Results -- #
 # -- ------------------------------------------------------------- ------------------------------------- -- #
 
-# get data for prices and predictions
+# Get data for prices and predictions
 ohlc_prices = data
 
 ohlc_class = {'train_y': global_model['model']['results']['data']['train']['y_train'],
@@ -176,11 +182,14 @@ ohlc_class = {'train_y': global_model['model']['results']['data']['train']['y_tr
               'test_y': global_model['model']['results']['data']['test']['y_test'],
               'test_y_pred': global_model['model']['results']['data']['test']['y_test_pred']}
 
-# plot title
+# Plot title
 dt.theme_plot_3['p_labels']['title'] = 'Global results with t-fold optimized parameters'
 
-# make plot
+# Make plot
 plot_5 = vs.g_ohlc_class(p_ohlc=ohlc_prices, p_theme=dt.theme_plot_3, p_data_class=ohlc_class, p_vlines=None)
 
-# visualize plot
+# Show plot in script
 plot_5.show()
+
+# Generate plot online with chartstudio
+# py.plot(plot_5)
