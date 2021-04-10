ebabdcbe0053d5176f7e8f3a9cab9cf4c9baf6c2
IFFranciscoME <if.francisco.me@gmail.com>
2021-04-10 15:10:29 -0500

    script with calculations for later jupyter notebook

---
+++ notebook.py
@@ -0,0 +1,11 @@
+
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- Project: Masters in Data Science Thesis Project                                                     -- #
+# -- Statistical Learning and Genetic Methods to Design, Optimize and Calibrate Trading Systems          -- #
+# -- File: notebook.py - python script with all the output elements for a jupyter notebook               -- #
+# -- Author: IFFranciscoME - if.francisco.me@gmail.com                                                   -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- Repository: https://github.com/IFFranciscoME/Msc_Thesis                                             -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
